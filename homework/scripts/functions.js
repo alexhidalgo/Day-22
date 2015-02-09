d28fd369bbbd51bd4d784e03ed7c644909f48779
Alex Hidalgo <aj1hidalgo@gmail.com>
2015-02-09 16:08:16 -0600

    Added function and test files

--- homework/scripts/functions.js
+++ homework/scripts/functions.js
@@ -2,7 +2,51 @@
 // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript.
 // ---------------------
 
-function max(){
+function max(a, b){
     "use strict";
+    if(!_.isNumber(a)) {
+		throw 'The first argument must be a number.';
+	}
+	 if(!_.isNumber(b)) {
+		throw 'The second argument must be a number.';
+	}
+	 if(arguments.length > 2 || arguments.length < 2) {
+		throw 'You must supply two arguments';
+	}
 
+  if(a > b) {
+  	return a;
+  } else {
+  	return b;
+  }
+}
+
+// ---------------------
+// Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
+// ---------------------
+
+function maxOfThree(a, b, c){
+    "use strict";
+    throw 'The first argument must be a number.';
+	}
+	 if(!_.isNumber(b)) {
+		throw 'The second argument must be a number.';
+	}
+	 if(!_.isNumber(c)) {
+		throw 'The third argument must be a number.';
+	}
+	 if(arguments.length > 3 || arguments.length < 3) {
+		throw 'You must supply three arguments';
+	}
+
+  if(a > b) {
+  	a = maxNum;
+  } else {
+  	b = maxNum;
+  }
+  if (c > maxNum) {
+  	return c;
+  } else {
+  	return maxNum;
+  }
 }
--- homework/tests/tests.js
+++ homework/tests/tests.js
@@ -5,13 +5,10 @@ var expect = chai.expect;
 // ---------------------
 
 describe('max', function() {
-	it('should not allow non-numeric arguments', funciton() {
-		expect(function() {
-			max('a', 1)
-		}).to.throw('The first argument must be a number.');
-		expect(function() {max()}).to.throw('You must supply two number arguments');
-		expect(function(){max('')}).to.throw('You must supply number arguments');
-		expect(function(){max(1, 2, 3)}).to.throw('You must supply two or less arguments');
+	it('should not allow non-numeric arguments', function() {
+		expect(function() {max('a', 1)}).to.throw('The first argument must be a number.');
+		expect(function() {max(1, 'b')}).to.throw('The second argument must be a number.');
+		expect(function(){max(1, 2, 3)}).to.throw('You must supply two arguments');
 	});
 
 	it('should return the max', function() {
@@ -21,3 +18,18 @@ describe('max', function() {
 		expect(max(0, 0)).to.equal(0);
 	});
 });
+
+describe('maxOfThree', function(){
+	it('should not allow non-numeric arguments', function() {
+		expect(function() {maxOfThree('a', 1, 2)}).to.throw('The first argument must be a number.');
+		expect(function() {maxOfThree(1, 'b', 2)}).to.throw('The second argument must be a number.');
+		expect(function(){maxOfThree(2, 3, 'c')}).to.throw('The third argument must be a number.');
+	});
+
+	it('should return the max', function() {
+		expect(maxOfThree(1, 2, 3)).to.equal(3);
+		expect(maxOfThree(0.1, 0.2, 0.3)).to.equal(0.3);
+		expect(maxOfThree(-2, -1, 0)).to.equal(0);
+		expect(maxOfThree(0, 0, 0)).to.equal(0);
+	});
+})
